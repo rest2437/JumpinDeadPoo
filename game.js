c72060c61f0cf162d06f16943940d256dd8686c9
rest2437 <nocturnalrd@gmail.com>
2021-08-20 15:23:24 -0700

    changes, added sprite, background, player function, and fixed a couple errors

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 /***
  * @todo create canvas *******
- * @todo create player
+ * @todo create player *******
  * @todo make player jump and move left to right
  * @todo if player falls, player dies
  * @tood if player hit enemy, player dies
@@ -14,17 +14,18 @@
 let game = document.querySelector("#game");
 //console.log(game);
 let dude;
+let platforms;
 let isGameOver = false;
-let platformCount = 5;
-let platforms = [];
+// let platformCount = 5;
+// let platforms = [];
 
 // ====================== SETUP FOR CANVAS RENDERING ======================= //
 const ctx = game.getContext("2d");
-ctx.fillerStyle = "white";
-ctx.strokeStyle = "red";
-ctx.lineWidth = 5;
-ctx.fillRect(10, 10, 100, 100);
-ctx.strokeRect(10, 10, 100, 100);
+// ctx.fillerStyle = "white";
+// ctx.strokeStyle = "red";
+// ctx.lineWidth = 5;
+// ctx.fillRect(10, 10, 100, 100);
+// ctx.strokeRect(10, 10, 100, 100);
 
 game.setAttribute("class", "main-game");
 game.setAttribute("height", getComputedStyle(game)["height"]);
@@ -52,68 +53,69 @@ class Dude {
 //   setTimeout(() => {
 //     let x = Math.floor(Math.random() * game.width) - 40;
 //     let y = Math.floor(Math.random() * game.height) - 80;
-//     shrek = new Dude(x, y, "orange", 40, 80);
+//     dude = new Dude(x, y, "orange", 40, 80);
 //   }, 1000);
 //   return true;
 // }
 class Platform {
-  constructor(newPlatformBottom) {
-    this.bottom = newPlatformBottom;
-    this.left = Math.random() * 100;
-    this.visual = document.createElement("div");
+  constructor(x, y, color, width, height) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.width = width;
+    this.height = height;
 
-    const visual = this.visual;
-    visual.classList.add("platform");
-    visual.style.left = this.left + "px";
-    visual.style.bottom = this.bottom + "px";
-    game.appendChild(visual);
+    this.render = function () {
+      ctx.fillstyle = this.color;
+      ctx.fillRect = (this.x, this.y, this.width, this.height);
+    };
   }
 }
 function createPlatforms() {
-  for (let i = 0; i < platformCount; i++) {
-    let platformGap = 640 / platformCount;
-    let newPlatformBottom = 100 + i * platformGap;
-    let newPlatform = new Platform(newPlatformBottom);
-    platforms.push(newPlatform);
-    //console.log (platforms);
-  }
+  dude.alive = true;
+  setTimeout(() => {
+    let x = Math.floor(Math.random() * game.width) - 40;
+    let y = Math.floor(Math.random() * game.height) - 80;
+    platforms = new Platform(300, 400, "green", 85, 15);
+  }, 1000);
 }
 // ====================== HELPER FUNCTIONS ======================= //
 
 //======================= EVENT LISTENERS=========================//
 window.addEventListener("DOMContentLoaded", (e) => {
-  dude = new Dude(10, 20, "orange", 40, 80);
+  dude = new Dude(295, 520, "orange", 60, 85);
+  platforms = new Platform(300, 400, "green", 85, 15);
   document.addEventListener("keydown", movementHandler);
-  const runGame = setInterval(gameLoop, 120);
+  const runGame = setInterval(gameLoop, 80);
   //   movementHandler(e) //
 });
 
 // SANDBOX FOR TESTING PAINTING TECHNIQUES//
 
 //  GUIw //
-function movePlatforms() {
-  if (dudeBottomSpace > 200) {
-    platforms.forEach((platform) => {
-      platform.bottom -= 4;
-      let visual = platform.visual;
-      visual.style.bottom = platform.bottom + "px";
-    });
-  }
-}
+// function movePlatforms() {
+//   if (dudeBottomSpace > 200) {
+//     platforms.forEach((platform) => {
+//       platform.bottom -= 4;
+//       let visual = platform.visual;
+//       visual.style.bottom = platform.bottom + "px";
+//     });
+//   }
+// }
 //  KEYBOARD INTERACTION LOGIC //
 function movementHandler(e) {
   switch (e.keyCode) {
     case 87: //up
-      dude.y - 10 >= 0 ? (dude.y -= 10) : null;
+      dude.y - 20 >= 0 ? (dude.y -= 10) : null;
       break;
     case 65: //left
-      dude.x - 10 >= 0 ? (dude.x -= 10) : null;
+      dude.x - 20 >= 0 ? (dude.x -= 10) : null;
       break;
     case 83: //down
-      dude.y + 10 <= game.height ? (dude.y += 10) : null;
+      dude.y + 20 <= game.height ? (dude.y += 10) : null;
       break;
     case 68: //right
-      dude.x + 10 <= game.width ? (dude.x += 10) : null;
+      dude.x + 20 <= game.width ? (dude.x += 10) : null;
       break;
   }
 }
@@ -121,16 +123,10 @@ function movementHandler(e) {
 
 function gameLoop() {
   ctx.clearRect(0, 0, game.width, game.height);
-  //@todo add score
-  //display the X and Y coordinates of our hero
-  // movementDisplay.textContent = `X; ${hero.x} Y: ${hero.y}`;
-  // Check of the ogre is alive
   if (dude.alive) {
     dude.render();
-    // //detectHit
-    // let hit = detechHit(hero, shrek);
   }
-  // hero.render();
+  //platform.render();
 }
 function jump() {
   clearInterval(downTimerId);
@@ -173,3 +169,15 @@ function gameOver() {
 // ====================== COLLISION DETECTION ======================= //
 
 // ====================== PAINT INTIAL SCREEN ======================= //
+
+//old code
+// function createPlatforms() {
+//   setTimeout(() => {
+//     for (let i = 0; i < platformCount; i++) {
+//       let x = Math.floor(Math.random() * game.width) - 40;
+//       let y = Math.floor(Math.random() * game.height) - 80;
+//       platforms = new Platform(300, 400, "green", 85, 15);
+//     }
+//   }, 1000);
+// }
+// console.log(platforms);
